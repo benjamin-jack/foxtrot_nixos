8e69a43aa7511ae268fc7ee234fcfa29888107d6
benjamin-jack <ben@outfoxit.nz>
2024-06-16 19:43:17 +1200

    shortcut test

--- config.def.h
+++ config.def.h
@@ -60,12 +60,11 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st", NULL };
-static const char *mixercmd[] = { "st -e pulsemixer", NULL };
 static const char *explorercmd[] = {"thunar", NULL };
 
 static const Key keys[] = {
 	/* modifier                     key        function        argument */
-	{ WINKEY,			XK_s,	   spawn,	   {.v = mixercmd} },
+	{ WINKEY,			XK_s,	   spawn,	   {.v = {"st","-e","pulsmixer",NULL } } },
 	{ WINKEY,			XK_f,	   spawn,	   {.v = explorercmd } },
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
